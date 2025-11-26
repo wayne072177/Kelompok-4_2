163b33079f28269d95029dd199e5e530ff59e4eb
Arxd142 <arnoldymiracle11@gmail.com>
2025-11-26 14:36:49 +0800

    Menghitung harga setelah diskon.py

--- # Menghitung harga setelah diskon.py
+++ # Menghitung harga setelah diskon.py
@@ -2,7 +2,9 @@
 
 harga = float(input("Masukkan harga awal: "))
 diskon_persen = float(input("Masukkan diskon(%): "))
+
 diskon_rp = harga * (diskon_persen / 100)
 harga_setelah_diskon = harga - diskon_rp
+
 
 

dac2dea36d23c6e6804c2af614a87eac6d62cab1
cherieee <keyciamanarisip959@gmail.com>
2025-11-26 15:47:51 +0800

    Enhance discount calculation output display
    Added output statements to display the results of the discount calculation.

--- # Menghitung harga setelah diskon.py
+++ # Menghitung harga setelah diskon.py
@@ -5,3 +5,9 @@ diskon_persen = float(input("Masukkan diskon(%): "))
 
 
 
+
+print("\n=== HASIL PERHITUNGAN DISKON ===")
+print("Harga Awal : ", harga)
+print("Diskon     : ", diskon_persen, "%")
+print("Potongan Harga : ", diskon_rp)
+print("Harga setelah diskon:", harga_setelah_diskon)
